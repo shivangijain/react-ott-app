fac5c130f0a52c19c5791aed7640453c975e44fa
Shivangi Jain <jain.shivangi.20@gmail.com>
2023-05-02 14:28:09 +0530

    add root url

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,3 +4,4 @@ export const SET_SEARCH_QUERY = 'SET_SEARCH_QUERY';
 export const SET_PAGE_NUMBER = 'SET_PAGE_NUMBER';
 export const INITIAL_PAGE = 1;
 export const MISSING_IMAGE_PATH = 'slices/placeholder_for_missing_posters.png';
+export const ROOT_URL = 'https://shivangijain.github.io/react-ott-app'
